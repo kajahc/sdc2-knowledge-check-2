f83db71f68772a6579acbd5fd82fa26a9f9a7d77
Kajah Means <54476653+kajahc@users.noreply.github.com>
2023-10-28 15:09:36 -0400

    Completed Knowledge Check
    Issues pushing through command line, noted in the Slack channel

---
+++ sdc2-knowledge-check-2.sql
@@ -0,0 +1,9 @@
+﻿#3.
+Select CustomerID From Orders Where OrderID=10310 /*returns the customerID of 77*/
+Union /*look in table 2*/
+Select CustomerName From Customers where CustomerID=77; /*returns customerName for Customer ID 77*/
+
+#4.
+Select SupplierID From Products where ProductID=40 /*returns the SupplierID of */
+Union /*look in table 2*/
+Select Address from Suppliers where SupplierID=19; /*returns CustomerName for Customer ID 19*/
